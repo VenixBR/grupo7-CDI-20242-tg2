e7f41c7bab45d302c77c905520a5c4719bc8287c
mathiasmichelotti <mathias.michelotti@ecomp.ufsm.br>
2024-12-08 17:45:33 -0300

    Update Script_to_Create_Tables.sql

--- TG2/Script_to_Create_Tables.sql
+++ TG2/Script_to_Create_Tables.sql
@@ -5,19 +5,19 @@ CREATE TABLE Biblioteca (
   Sigla VARCHAR(10) NOT NULL
 );
 
-INSERT INTO Biblioteca (Cod_Biblioteca, Endereco, Nome)
+INSERT INTO Biblioteca (Cod_Biblioteca, Endereco, Nome, Sigla)
 VALUES 
-  (0, 'Street 1', 'Biblioteca Central'),
-  (1, 'Street 2', 'Biblioteca Setorial do Centro de Tecnologia'),
-  (2, 'Street 3', 'Biblioteca Setorial do Centro de Ciências Rurais'),
-  (3, 'Street 4', 'Biblioteca Setorial do Centro de Ciências Sociais e Humanas'),
-  (4, 'Street 5', 'Biblioteca Setorial do Centro de Artes e Letras'),
-  (5, 'Street 6', 'Biblioteca Setorial do Centro de Ciências Naturais e Exatas'),
-  (6, 'Street 7', 'Biblioteca Setorial do Centro do Colégio Técnico Industrial de Santa Maria'),
-  (7, 'Street 8', 'Biblioteca Setorial do Centro de Educação Física e Desporto'),
-  (8, 'Street 9', 'Biblioteca Setorial do Centro de Educação'),
-  (9, 'Street 10', 'Biblioteca Setorial do Centro do Colégio Politécnico'),
-  (10, 'Street 11', 'Biblioteca Setorial do Centro de Saúde');
+  (01, 'Street 1', 'Biblioteca Central', 'BC'),
+  (02, 'Street 2', 'Biblioteca Setorial do Centro de Tecnologia', 'BSCT'),
+  (03, 'Street 3', 'Biblioteca Setorial do Centro de Ciências Rurais', 'BSCR'),
+  (04, 'Street 4', 'Biblioteca Setorial do Centro de Ciências Sociais e Humanas', 'BSCCSH'),
+  (05, 'Street 5', 'Biblioteca Setorial do Centro de Artes e Letras', 'BSCAL'),
+  (06, 'Street 6', 'Biblioteca Setorial do Centro de Ciências Naturais e Exatas', 'BSCCNE'),
+  (07, 'Street 7', 'Biblioteca Setorial do Centro do Colégio Técnico Industrial de Santa Maria', 'BSCTISM'),
+  (08, 'Street 8', 'Biblioteca Setorial do Centro de Educação Física e Desporto', 'BSCEFD'),
+  (09, 'Street 9', 'Biblioteca Setorial do Centro de Educação', 'BSCE'),
+  (10, 'Street 10', 'Biblioteca Setorial do Centro do Colégio Politécnico', 'BSPOLI'),
+  (11, 'Street 11', 'Biblioteca Setorial do Centro de Saúde', 'BSCS');
 
 
 
@@ -29,15 +29,15 @@ CREATE TABLE Centro (
 
 INSERT INTO Centro (Cod_Centro, Sigla, Nome)
 VALUES 
-  (1, 'CT', 'Centro de Tecnologia'),
-  (2, 'CCR', 'Centro de Ciências Rurais'),
-  (3, 'CCSH', 'Centro de Ciências Sociais e Humanas'),
-  (4, 'CAL', 'Centro de Artes e Letras'),
-  (5, 'CCNE', 'Centro de Ciências Naturais e Exatas'),
-  (6, 'CTISM', 'Colégio Técnico Industrial de Santa Maria'),
-  (7, 'CEFD', 'Centro de Educação Física e Desporto'),
-  (8, 'CE', 'Centro de Educação'),
-  (9, 'POLI', 'Colégio Politécnico'),
+  (01, 'CT', 'Centro de Tecnologia'),
+  (02, 'CCR', 'Centro de Ciências Rurais'),
+  (03, 'CCSH', 'Centro de Ciências Sociais e Humanas'),
+  (04, 'CAL', 'Centro de Artes e Letras'),
+  (05, 'CCNE', 'Centro de Ciências Naturais e Exatas'),
+  (06, 'CTISM', 'Colégio Técnico Industrial de Santa Maria'),
+  (07, 'CEFD', 'Centro de Educação Física e Desporto'),
+  (08, 'CE', 'Centro de Educação'),
+  (09, 'POLI', 'Colégio Politécnico'),
   (10, 'CCS', 'Centro de Ciências da Saúde');
 
 
@@ -50,9 +50,9 @@ CREATE TABLE Pertence (
 
 INSERT INTO Pertence (fk_Cod_Biblioteca, fk_Cod_Centro)
 VALUES
-  (1, 1),
-  (2, 2),
-  (3, 3);
+  (01, 01),
+  (02, 02),
+  (03, 03);
 
 
 CREATE TABLE Aluno (
@@ -65,21 +65,21 @@ CREATE TABLE Aluno (
 
 INSERT INTO Aluno (Matricula, Endereco, Nome, fk_Cod_Centro)
 VALUES
-  (202220000, 'Avenida Augusta', 'José Mourinho', 1),
-  (202220001, 'Avenida Paulista', 'Carlos Ancelotti', 2),
-  (202220002, 'Rua das Flores', 'Pep Guardiola', 3),
-  (202220003, 'Rua do Sol', 'Roger Machado', 4),
-  (202220004, 'Avenida Brasil', 'Zinedine Zidane', 5),
-  (202220005, 'Rua da Liberdade', 'Diego Simeone', 6),
-  (202220006, 'Rua do Porto', 'Renato Gaúcho', 7),
-  (202220007, 'Rua Rio de Janeiro', 'Antonio Conte', 8),
-  (202220008, 'Avenida São João', 'Arsène Wenger', 9),
+  (202220000, 'Avenida Augusta', 'José Mourinho', 01),
+  (202220001, 'Avenida Paulista', 'Carlos Ancelotti', 02),
+  (202220002, 'Rua das Flores', 'Pep Guardiola', 03),
+  (202220003, 'Rua do Sol', 'Roger Machado', 04),
+  (202220004, 'Avenida Brasil', 'Zinedine Zidane', 05),
+  (202220005, 'Rua da Liberdade', 'Diego Simeone', 06),
+  (202220006, 'Rua do Porto', 'Renato Gaúcho', 07),
+  (202220007, 'Rua Rio de Janeiro', 'Antonio Conte', 08),
+  (202220008, 'Avenida São João', 'Arsène Wenger', 09),
   (202220009, 'Rua do Carmo', 'Frank Lampard', 10),
-  (202220010, 'Rua das Palmeiras', 'David Moyes', 1),
-  (202220011, 'Avenida dos Eucaliptos', 'Claudio Ranieri', 2),
-  (202220012, 'Rua da Paz', 'Marcelo Bielsa', 3),
-  (202220013, 'Rua do Parque', 'Tite', 4),
-  (202220014, 'Avenida do Mar', 'Luis Enrique', 5);
+  (202220010, 'Rua das Palmeiras', 'David Moyes', 01),
+  (202220011, 'Avenida dos Eucaliptos', 'Claudio Ranieri', 02),
+  (202220012, 'Rua da Paz', 'Marcelo Bielsa', 03),
+  (202220013, 'Rua do Parque', 'Tite', 04),
+  (202220014, 'Avenida do Mar', 'Luis Enrique', 05);
 
 
 
@@ -91,15 +91,15 @@ CREATE TABLE Funcionario (
 
 INSERT INTO Funcionario (Cod_Funcionario, Salario, Nome)
 VALUES
-  (1, '9398.98', 'Lionel Messi'),
-  (2, '10000.00', 'Cristiano Ronaldo'),
-  (3, '9500.50', 'Neymar Jr.'),
-  (4, '8700.75', 'Kylian Mbappé'),
-  (5, '11000.00', 'Luka Modrić'),
-  (6, '9900.20', 'Sergio Ramos'),
-  (7, '9600.35', 'Zlatan Ibrahimović'),
-  (8, '9200.60', 'Kevin De Bruyne'),
-  (9, '9400.80', 'Mohamed Salah'),
+  (01, '9398.98', 'Lionel Messi'),
+  (02, '10000.00', 'Cristiano Ronaldo'),
+  (03, '9500.50', 'Neymar Jr.'),
+  (04, '8700.75', 'Kylian Mbappé'),
+  (05, '11000.00', 'Luka Modrić'),
+  (06, '9900.20', 'Sergio Ramos'),
+  (07, '9600.35', 'Zlatan Ibrahimović'),
+  (08, '9200.60', 'Kevin De Bruyne'),
+  (09, '9400.80', 'Mohamed Salah'),
   (10, '10500.90', 'Robert Lewandowski'),
   (11, '9800.40', 'Virgil van Dijk'),
   (12, '9150.30', 'Harry Kane'),
@@ -116,15 +116,15 @@ CREATE TABLE Autor (
 
 INSERT INTO Autor (Cod_Autor, Nome, Pais)
 VALUES
-  (1, 'J.K. Rowling', 'Reino Unido'),
-  (2, 'George Orwell', 'Reino Unido'),
-  (3, 'Haruki Murakami', 'Japão'),
-  (4, 'Gabriel García Márquez', 'Colômbia'),
-  (5, 'Leo Tolstoy', 'Rússia'),
-  (6, 'Franz Kafka', 'Áustria'),
-  (7, 'Jane Austen', 'Reino Unido'),
-  (8, 'Mark Twain', 'Estados Unidos'),
-  (9, 'Chimamanda Ngozi Adichie', 'Nigéria'),
+  (01, 'J.K. Rowling', 'Reino Unido'),
+  (02, 'George Orwell', 'Reino Unido'),
+  (03, 'Haruki Murakami', 'Japão'),
+  (04, 'Gabriel García Márquez', 'Colômbia'),
+  (05, 'Leo Tolstoy', 'Rússia'),
+  (06, 'Franz Kafka', 'Áustria'),
+  (07, 'Jane Austen', 'Reino Unido'),
+  (08, 'Mark Twain', 'Estados Unidos'),
+  (09, 'Chimamanda Ngozi Adichie', 'Nigéria'),
   (10, 'Ernest Hemingway', 'Estados Unidos');
 
 
@@ -140,13 +140,13 @@ CREATE TABLE Publicacao (
 
 INSERT INTO Publicacao (Cod_Publicacao, Tipo, Ano, Nome, fk_Cod_Biblioteca)
 VALUES
-  (1, 'Acadêmico', '2020-01-15', 'Introdução à Ciência de Dados', 1),
-  (2, 'Acadêmico', '2019-11-22', 'Matemática Avançada', 2),
-  (3, 'Acadêmico', '2018-08-13', 'Fundamentos de Engenharia', 3),
-  (4, 'Literatura', '2021-05-17', 'Romance Clássico', 4),
-  (5, 'Autoajuda', '2022-06-19', 'O Poder do Hábito', 5),
-  (6, 'Literatura', '2017-02-08', 'O Senhor dos Anéis', 6),
-  (7, 'Acadêmico', '2020-07-30', 'Física para Engenheiros', 7);
+  (01, 'Acadêmico', '2020-01-15', 'Introdução à Ciência de Dados', 01),
+  (02, 'Acadêmico', '2019-11-22', 'Matemática Avançada', 02),
+  (03, 'Acadêmico', '2018-08-13', 'Fundamentos de Engenharia', 03),
+  (04, 'Literatura', '2021-05-17', 'Romance Clássico', 04),
+  (05, 'Autoajuda', '2022-06-19', 'O Poder do Hábito', 05),
+  (06, 'Literatura', '2017-02-08', 'O Senhor dos Anéis', 06),
+  (07, 'Acadêmico', '2020-07-30', 'Física para Engenheiros', 07);
 
 
 CREATE TABLE Academico (
@@ -158,13 +158,13 @@ CREATE TABLE Academico (
 
 INSERT INTO Academico (Edicao, Area, fk_Cod_Publicacao)
 VALUES
-  (1, 'Ciência de Dados', 1),
-  (2, 'Matemática', 2),
-  (3, 'Engenharia Elétrica', 3),
-  (4, 'Romance', 4),
-  (5, 'Psicologia', 5),
-  (6, 'Ficção Fantástica', 6),
-  (7, 'Física', 7);
+  (01, 'Ciência de Dados', 01),
+  (02, 'Matemática', 02),
+  (03, 'Engenharia Elétrica', 03),
+  (04, 'Romance', 04),
+  (05, 'Psicologia', 05),
+  (06, 'Ficção Fantástica', 06),
+  (07, 'Física', 07);
 
 
 
@@ -176,9 +176,9 @@ CREATE TABLE Literatura (
 
 INSERT INTO Literatura (Genero_Textual, fk_Cod_Publicacao)
 VALUES
-  ('Ficção Científica', 4),
-  ('Romance', 6),
-  ('Aventura', 7);
+  ('Ficção Científica', 04),
+  ('Romance', 06),
+  ('Aventura', 07);
 
 
 
@@ -203,13 +203,13 @@ CREATE TABLE Escrito (
 
 INSERT INTO Escrito (fk_Cod_Publicacao, fk_Cod_Autor)
 VALUES
-  (1, 1),
-  (2, 2),
-  (3, 3),
-  (4, 7),
-  (5, 4),
-  (6, 6),
-  (7, 8);
+  (01, 01),
+  (02, 02),
+  (03, 03),
+  (04, 07),
+  (05, 04),
+  (06, 06),
+  (07, 08);
 
 
 CREATE TABLE Emprestimo (
@@ -226,10 +226,10 @@ CREATE TABLE Emprestimo (
 
 INSERT INTO Emprestimo (Cod_Emprestimo, Data_, Hora, fk_Cod_Publicacao, fk_Matricula, fk_Cod_Funcionario)
 VALUES
-  (1, '2024-01-12', '14:30:00', 1, 202220000, 1),
-  (2, '2024-01-13', '10:00:00', 2, 202220001, 2),
-  (3, '2024-01-14', '16:15:00', 3, 202220002, 3),
-  (4, '2024-02-20', '11:20:00', 4, 202220003, 4),
-  (5, '2024-02-21', '14:45:00', 5, 202220004, 5),
-  (6, '2024-03-05', '09:30:00', 6, 202220005, 6),
-  (7, '2024-03-10', '13:00:00', 7, 202220006, 7);
+  (01, '2024-01-12', '14:30:00', 1, 202220000, 01),
+  (02, '2024-01-13', '10:00:00', 2, 202220001, 02),
+  (03, '2024-01-14', '16:15:00', 3, 202220002, 03),
+  (04, '2024-02-20', '11:20:00', 4, 202220003, 04),
+  (05, '2024-02-21', '14:45:00', 5, 202220004, 05),
+  (06, '2024-03-05', '09:30:00', 6, 202220005, 06),
+  (07, '2024-03-10', '13:00:00', 7, 202220006, 07);
