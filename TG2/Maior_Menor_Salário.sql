57d36f755d1cffa85c705dd451da9c9a2b6bfd5b
mathiasmichelotti <mathias.michelotti@ecomp.ufsm.br>
2024-12-08 18:26:12 -0300

    Update Maior_Menor_Salário.sql

--- TG2/Maior_Menor_Salário.sql
+++ TG2/Maior_Menor_Salário.sql
@@ -1,4 +1,4 @@
-SELECT Nome, Salario
+SELECT Nome, cod_funcionario, Salario
 FROM Funcionario
 WHERE Salario = (SELECT MAX(Salario) FROM Funcionario)
    OR Salario = (SELECT MIN(Salario) FROM Funcionario);
